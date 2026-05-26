c7c4b6a4aaad2bffda11df4b6bfcc5fd4a63c89f
King LaD <fredsomo2005@gmail.com>
2026-05-26 08:44:21 +0100

    Fix mixed content by forcing https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        URL::forceScheme('https');
+        // Force Laravel à utiliser HTTPS pour tous les liens générés
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
